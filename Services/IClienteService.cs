4fab9104bdb80201c74c83276a4f9975786df99c
Victor Garcia <vgarcia@payall.com.ve>
2025-07-23 11:03:29 -0400

    ✨ Agregar metodo PATCH para actualizar parcialmente clientes

--- Services/IClienteService.cs
+++ Services/IClienteService.cs
@@ -10,5 +10,6 @@ namespace devsu.Services
         Task<ClienteDto> CreateClienteAsync(ClienteDto clienteDto);
         Task<IEnumerable<ClienteDto>> GetAllClientesAsync();
         Task<ClienteDto> UpdateClienteAsync(int id, ClienteDto clienteDto);
+        Task<ClienteDto> PatchClienteAsync(int id, ClientePatchDto clientePatchDto);
     }
 }
